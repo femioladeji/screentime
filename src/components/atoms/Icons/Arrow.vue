ed5b08faa3e5ea16acc7b6a2a40e2a9035f4f754
Femi Oladeji <femidotexe@gmail.com>
2021-10-01 00:58:17 +0200

    allow save for basic settings

---
+++ src/components/atoms/Icons/Arrow.vue
@@ -0,0 +1,21 @@
+<!--eslint-disable max-len-->
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 10.8281L16.95 15.7781L18.364 14.3641L12 8.00008L5.63599 14.3641L7.04999 15.7781L12 10.8281Z"
+      fill="#4F4F4F"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'ArrowUpIcon'
+};
+</script>

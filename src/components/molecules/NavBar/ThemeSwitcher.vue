3761b7e62bc7d2368e07f764c1ad01e05d704eae
Femi Oladeji <femidotexe@gmail.com>
2021-10-02 22:53:07 +0200

    remove close extension icon

--- src/components/molecules/NavBar/ThemeSwitcher.vue
+++ src/components/molecules/NavBar/ThemeSwitcher.vue
@@ -5,15 +5,11 @@
     </switch-button>
     <light v-if="isLightTheme" class="theme-image" />
     <dark v-else class="theme-image" />
-    <button class="close" @click="close">
-      <close />
-    </button>
   </div>
 </template>
 
 <script>
 import SwitchButton from '../../atoms/Switch';
-import Close from '../../atoms/Icons/Close';
 import Dark from '../../atoms/Icons/Dark';
 import Light from '../../atoms/Icons/Light';
 import utils, { SETTINGSKEY } from '../../../assets/js/utils';
@@ -22,7 +18,6 @@ export default {
   name: 'ThemeSwitcher',
   components: {
     SwitchButton,
-    Close,
     Light,
     Dark
   },
@@ -52,9 +47,6 @@ export default {
         body.classList.remove('dark-mode');
       }
       utils.saveConfiguration(SETTINGSKEY, { theme });
-    },
-    close() {
-      console.log('close');
     }
   },
   async mounted() {
